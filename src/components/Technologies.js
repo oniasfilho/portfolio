13e01bd24e0c7ad5164cfcc9d1eb77f23da26ae2
oniasfilho <onias.filho@gmail.com>
2023-02-12 09:48:40 -0400

    finishes technologies section

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+
 
 const Technologies = () => {
 	return (
-		<div>Technologies</div>
-	)
+		<div className='technologies-wrapper'>
+			<div className='skill-wrapper'>
+				<p className='skill-name'>Javascript</p>
+				<p className='skill-exp'>3 Years Experience</p>
+			</div>
+			<div className='skill-wrapper'>
+				<p className='skill-name'>React</p>
+				<p className='skill-exp'>3 Years Experience</p>
+			</div>
+			<div className='skill-wrapper'>
+				<p className='skill-name'>CSS</p>
+				<p className='skill-exp'>3 Years Experience</p>
+			</div>
+			<div className='skill-wrapper'>
+				<p className='skill-name'>Java</p>
+				<p className='skill-exp'>2 Years Experience</p>
+			</div>
+			<div className='skill-wrapper'>
+				<p className='skill-name'>Spring Framework</p>
+				<p className='skill-exp'>2 Years Experience</p>
+			</div>
+			<div className='skill-wrapper'>
+				<p className='skill-name'>SQL</p>
+				<p className='skill-exp'>3 Years Experience</p>
+			</div>
+		</div>
+	);
 }
 
 export default Technologies
