6f6ce851fe7a3f6e63b97743ee5e306bfaddd12e
Onias da Rocha <onias.filho@gmail.com>
2023-02-14 21:38:07 -0400

    finishes form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
+
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		message: "",
+	})
+
+	const [invalidFields, setInvalidFields] = useState([])
+
+	const handleChangeForm = (e) => {
+		const { name, value } = e.target;
+		setForm((prevForm) => ({
+			...prevForm,
+			[name]: value
+		}));
+	}
+
+	const handleSendMessage = () => {
+		const invalidFields = [];
+		if (!form.name) {
+			invalidFields.push("name");
+		}
+		if (!form.email) {
+			invalidFields.push("email");
+		}
+		if (!/\S+@\S+\.\S+/.test(form.email)) {
+			invalidFields.push("email");
+		}
+		if (!form.message) {
+			invalidFields.push("message");
+		}
+		setInvalidFields(invalidFields);
+		if (invalidFields.length === 0) {
+			// If all fields are valid, submit the form
+			console.log("Form submitted:", form);
+		}
+	}
+
 	return (
 		<div className="contact-wrapper">
 			<div className="inner-contact-wrapper">
@@ -19,15 +57,50 @@ const Contact = () => {
 						<div className="form-wrapper">
 							<form action="/submit-form" method="post">
 								<div className="form-input">
-									<input type="text" id="name" name="name" placeholder='Name' required />
-									<input type="email" id="email" name="email" placeholder='Email' required />
-									<textarea id="message" name="message" placeholder='Message' required></textarea>
+									<input
+										type="text"
+										id="name"
+										name="name"
+										placeholder='Name'
+										onChange={handleChangeForm}
+										value={form.name}
+										className={invalidFields.includes("name") ? "invalid-field" : ""}
+									/>
+									{invalidFields.includes("name") && (
+										<p className="error-message">Sorry, invalid format here</p>
+									)}
+									<input
+										type="email"
+										id="email"
+										name="email"
+										placeholder='Email'
+										onChange={handleChangeForm}
+										value={form.email}
+										className={invalidFields.includes("email") ? "invalid-field" : ""}
+									/>
+									{invalidFields.includes("email") && (
+										<p className="error-message">Sorry, invalid format here</p>
+									)}
+									<textarea
+										id="message"
+										name="message"
+										placeholder='Message'
+										onChange={handleChangeForm}
+										value={form.message}
+										className={invalidFields.includes("message") ? "invalid-field" : ""}
+										required
+									>
+
+										{invalidFields.includes("message") && (
+											<p className="error-message">Sorry, invalid format here</p>
+										)}
+									</textarea>
 								</div>
 							</form>
 						</div>
 						<div className="submit-button-wrapper">
 							<div className='message-me-button'>
-								<span className='underlined-portion-sm'>send message</span>
+								<span onClick={handleSendMessage} className='underlined-portion-sm'>send message</span>
 							</div>
 						</div>
 					</div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,14 @@ const Projects = () => {
 						<div className='single-project-wrapper' key={id}>
 							<div
 								className={`project-image-section project-1 ${each.className}`}
-							></div>
+							>
+								<div className="project-overlay">
+									<div className="project-links">
+										<p className='overlay-links'>View Project</p>
+										<a className='overlay-links'>View Code</a>
+									</div>
+								</div>
+							</div>
 							<p className='project-title'>{each.projectTitle}</p>
 							<div className={`project-technologies-tags`}>
 								{each.tags.map((tag, tagId) => {
